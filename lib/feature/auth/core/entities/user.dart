a7223a2a175e5b0b80e6333be6de61ed19f392fc
SoffiaSanchezz <ssofiiadayanna@gmail.com>
2026-01-23 19:47:43 -0500

    config(core): configure Dio client per platform and add user serialization

--- lib/feature/auth/core/entities/user.dart
+++ lib/feature/auth/core/entities/user.dart
@@ -1,9 +1,35 @@
 class User {
   final String id;
   final String email;
+  final String username;
+  final bool isVerified;
+  final bool isActive;
 
   const User({
     required this.id,
     required this.email,
+    required this.username,
+    required this.isVerified,
+    required this.isActive,
   });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'] ?? '',
+      email: json['email'] ?? '',
+      username: json['username'] ?? '',
+      isVerified: json['is_verified'] ?? false,
+      isActive: json['is_active'] ?? false,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'email': email,
+      'username': username,
+      'is_verified': isVerified,
+      'is_active': isActive,
+    };
+  }
 }
