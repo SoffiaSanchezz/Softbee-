45312007e4f3c5486fd6eadb8a0a289dbebc5c5f
SoffiaSanchezz <ssofiiadayanna@gmail.com>
2026-01-23 19:45:03 -0500

    feat(auth): persist token and user using shared preferences and local datasource

--- lib/feature/auth/data/datasources/auth_local_datasource.dart
+++ lib/feature/auth/data/datasources/auth_local_datasource.dart
@@ -1,25 +1,58 @@
-// Placeholder for local authentication data source
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import '../../core/entities/user.dart'; // Importar el modelo User
+
 abstract class AuthLocalDataSource {
   Future<void> saveToken(String token);
   Future<String?> getToken();
   Future<void> deleteToken();
+  Future<void> saveUser(User user);
+  Future<User?> getUser();
+  Future<void> deleteUser();
 }
 
 class AuthLocalDataSourceImpl implements AuthLocalDataSource {
-  // TODO: Implement local data source logic using shared_preferences or flutter_secure_storage
+  static const String _authTokenKey = 'auth_token';
+  static const String _authUserKey = 'auth_user';
+
   @override
   Future<void> deleteToken() async {
-    // TODO: implement deleteToken
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_authTokenKey);
   }
 
   @override
   Future<String?> getToken() async {
-    // TODO: implement getToken
-    return null;
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_authTokenKey);
   }
 
   @override
   Future<void> saveToken(String token) async {
-    // TODO: implement saveToken
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_authTokenKey, token);
+  }
+
+  @override
+  Future<void> saveUser(User user) async {
+    final prefs = await SharedPreferences.getInstance();
+    final userJson = json.encode(user.toJson()); // Necesitaremos un toJson en User
+    await prefs.setString(_authUserKey, userJson);
+  }
+
+  @override
+  Future<User?> getUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    final userJson = prefs.getString(_authUserKey);
+    if (userJson != null) {
+      return User.fromJson(json.decode(userJson));
+    }
+    return null;
+  }
+
+  @override
+  Future<void> deleteUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_authUserKey);
   }
 }
